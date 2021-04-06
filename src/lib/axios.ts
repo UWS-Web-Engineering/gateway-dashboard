1c27db34dda5dac1c6a45d1fc902ee95104ee37e
Yashar Zolmajdi <yashar.zolmajdi@gmail.com>
2021-04-06 12:27:42 +0700

    Fix api url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `${process.env.API_URL}/api`,
+  baseURL: `${process.env.API_URL}`,
   withCredentials: true,
   headers: {
     Authorization: `bearer ${window.sessionStorage.getItem("token")}`,
